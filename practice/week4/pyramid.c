e7907cf9e013012745e0d2b3e0a502a16bac2d1a
unknown <yuusoyean123@naver.com>
2023-09-25 15:47:46 +0900

    Week4.1. 반복문 실습 + 피라미드 쌓기

--- practice/week4/pyramid.c
+++ practice/week4/pyramid.c
@@ -4,13 +4,16 @@ int main()
 {
 	int floor;
 	printf("몇 층을 쌓겠습니까? (5~100)");
+	//입력한 숫자의 줄을 출력
 	scanf_s("%d", &floor);
 
 	for (int i = 0; i < floor ; i++) {
 		for (int j = 0; j < floor - 1 - i; j++) {
+			//숫자 - 1 -i 만큼 출력 
 			printf("S");
 		}
 		for (int k = 0; k < i * 2 + 1; k++) {
+			//숫자 i*2+1 만큼 *을 출력
 			printf("*");
 		}
 		printf("\n");
